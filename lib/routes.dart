eb230e23c0c8264142d6e4e4ae7da256359d3fa8
h.nomura <>
2024-05-12 01:35:46 +0900

    update: routerの修正

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tutorial/view/main/main_screen.dart';
+import 'package:flutter_tutorial/view/splash/splash_screen.dart';
 import 'package:go_router/go_router.dart';
 
 class Routes {
+  /// スプラッシュ画面
+  static const String root = '/';
+
   /// メイン画面
   static const String main = '/main';
 
   static final GoRouter router = GoRouter(
-    initialLocation: main,
+    initialLocation: root,
     routes: [
       GoRoute(
+        path: root,
+        name: root,
+        pageBuilder: (context, state) => const MaterialPage<void>(
+          child: SplashScreen(),
+        ),
+      ),
+      GoRoute(
         path: main,
+        name: main,
         pageBuilder: (context, state) {
           final MainScreenArguments arguments =
               state.extra! as MainScreenArguments;
