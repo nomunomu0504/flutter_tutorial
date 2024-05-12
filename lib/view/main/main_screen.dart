240d8f908ea08840292262d24a3a03acb61a01f8
h.nomura <>
2024-05-12 20:06:59 +0900

    feat: count保存ボタンと保存されているcountを表示するリストを作成

--- lib/view/main/main_screen.dart
+++ lib/view/main/main_screen.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_tutorial/provider/counter_provider.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_tutorial/provider/count_store/count_store_provider.dart';
+import 'package:flutter_tutorial/provider/counter/counter_provider.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class MainScreen extends ConsumerWidget {
@@ -13,7 +16,10 @@ class MainScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final counter = ref.watch(counterProvider);
-    final notifier = ref.read(counterProvider.notifier);
+    final savedCounts = ref.watch(counterStoreProvider);
+
+    final counterNotifier = ref.read(counterProvider.notifier);
+    final counterStoreNotifier = ref.read(counterStoreProvider.notifier);
 
     return Scaffold(
       appBar: AppBar(
@@ -31,13 +37,51 @@ class MainScreen extends ConsumerWidget {
               '$counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            Container(
+              height: 200,
+              width: 200,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black12, width: 1),
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  children: savedCounts
+                      .asMap()
+                      .entries
+                      .map(
+                        (entry) => Container(
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: entry.key % 2 == 0 ? null : Colors.black12,
+                          ),
+                          child: Text(
+                            entry.value.toString(),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: notifier.incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          FloatingActionButton(
+            onPressed: () => counterStoreNotifier.saveCount(counter),
+            tooltip: 'Save Count',
+            child: const Icon(Icons.save),
+          ),
+          const SizedBox(width: 16),
+          FloatingActionButton(
+            onPressed: counterNotifier.incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+        ],
       ),
     );
   }
