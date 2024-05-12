522ce786d2f7c1dae5da499edfab2106dd5132d0
h.nomura <>
2024-05-13 00:00:21 +0900

    feat: shared_preferencesにcountを保存するためのRepositoryを作成

---
+++ lib/repository/local/count_store/count_store_local_repository.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:flutter_tutorial/provider/shared_preferences/shared_preferences_provider.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+part 'count_store_local_repository.g.dart';
+
+@Riverpod(keepAlive: true)
+CountStoreLocalRepository countStoreLocalRepository(CountStoreLocalRepositoryRef ref) {
+  final sharedPreferences = ref.read(sharedPreferencesProvider);
+  return _CountStoreLocalRepository(
+    sharedPreferences: sharedPreferences,
+  );
+}
+
+abstract class CountStoreLocalRepository {}
+
+class _CountStoreLocalRepository implements CountStoreLocalRepository {
+  _CountStoreLocalRepository({
+    required SharedPreferences sharedPreferences,
+  }) : _sharedPreferences = sharedPreferences;
+
+  static const String _sharedPreferencesKey = 'sharedPreferences';
+  final SharedPreferences _sharedPreferences;
+
+  void saveCurrentStoredCounts(List<int> storedCounts) {
+    _sharedPreferences.setString(_sharedPreferencesKey, jsonEncode(storedCounts));
+  }
+
+  List<int> getCurrentStoredCounts() {
+    final storedCountsJsonString = _sharedPreferences.getString(_sharedPreferencesKey);
+    return storedCountsJsonString == null ? [] : jsonDecode(storedCountsJsonString);
+  }
+}
---
+++ lib/repository/local/count_store/count_store_local_repository.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'count_store_local_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$countStoreLocalRepositoryHash() =>
+    r'1dd70900cab339e5dda8c9de64140c856c698771';
+
+/// See also [countStoreLocalRepository].
+@ProviderFor(countStoreLocalRepository)
+final countStoreLocalRepositoryProvider =
+    Provider<CountStoreLocalRepository>.internal(
+  countStoreLocalRepository,
+  name: r'countStoreLocalRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$countStoreLocalRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef CountStoreLocalRepositoryRef = ProviderRef<CountStoreLocalRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
